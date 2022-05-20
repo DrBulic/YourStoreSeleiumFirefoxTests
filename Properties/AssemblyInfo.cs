edd5f1cba23ff4ba05d96f4f6efcd194f0966060
DrBulic <105888205+DrBulic@users.noreply.github.com>
2022-05-20 09:44:59 +0200

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumFirefox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Enghouse")]
+[assembly: AssemblyProduct("SeleniumFirefox")]
+[assembly: AssemblyCopyright("Copyright © Enghouse 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("991bc5d1-b687-43f2-bab1-3c12ff26ec07")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
